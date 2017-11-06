45e873fc09025b92bcbeb1cd10ecd7a473289e9f
OmaGash <francislaxasantos@gmail.com>
2017-11-06 18:34:48 +0800

    Implemented Jump and Gravity

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,18 +1,23 @@
 extends KinematicBody2D
 
-export var speed = 0
-var velocity
-var dir    = 0
-var to_dir = 0
+export var speed = Vector2()
+var velocity     = Vector2()
+var dir          = 0
+var to_dir       = 0
 
 const MAX_SPEED    = 300
 const ACCELERATION = 600
 const DECELERATION = 900
+const JUMP_FORCE   = 250
+const GRAVITY      = 800
 
 func _ready():
 	set_fixed_process(true)
 
 func _fixed_process(delta):
+	
+	if Input.is_action_pressed("move_jump"):
+		speed.y = -JUMP_FORCE
 	
 	if   Input.is_action_pressed("move_left"):
 		  dir = -1
@@ -21,11 +26,25 @@ func _fixed_process(delta):
 	else: dir =  0
 	
 	if dir != 0:
-		  speed += ACCELERATION * delta
+		  speed.x += ACCELERATION * delta
 		  to_dir = dir
-	else: speed -= DECELERATION * delta
+	else: speed.x -= DECELERATION * delta
 	
-	speed = clamp(speed , 0 , MAX_SPEED)
-	velocity = speed * to_dir * delta
+	speed.x = clamp(speed.x , 0 , MAX_SPEED)
 	
-	move(Vector2(velocity , 0))
+	speed.y += GRAVITY * delta
+	
+	velocity.x = speed.x * to_dir * delta
+	velocity.y = speed.y * delta
+	
+	move(velocity)
+	
+	var move_remainder = move(velocity)
+	
+	if is_colliding():
+		if get_collider().is_in_group("ground"):
+			var normal = get_collision_normal()
+			var final_movement = normal.slide(move_remainder)
+			speed.y = 0
+			move(final_movement)
+	
